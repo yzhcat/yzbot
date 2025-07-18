dbd4c9e59a013ef5916e79c53680601e4b865435
yzhcat <yzhcat@qq.com>
2025-07-18 15:07:43 +0800

    参数微调

--- my_send_goal0.sh
+++ my_send_goal0.sh
@@ -2,9 +2,9 @@ ros2 action send_goal /arm_controller/follow_joint_trajectory control_msgs/actio
   trajectory: {
     joint_names: [joint1, joint2, joint3, joint4, joint5, joint6],
     points: [
-      { positions: [0.0, 0.0, 0.0, 0.0, 0.0, 0.0], time_from_start: { sec: 0, nanosec: 500 } },
+      { positions: [0.0, 1.2, 1.15, 0.0, -0.3, 0.0], time_from_start: { sec: 0, nanosec: 500 } },
       { positions: [0.0, 0.9, 1.15, 0.0, -0.3, 0.0], time_from_start: { sec: 3, nanosec: 500 } },
-      { positions: [0.0, 1.2, 1.15, 0.0, -0.3, 0.0], time_from_start: { sec: 5, nanosec: 500 } },
+      { positions: [0.0, 0.0, 0.0, 0.0, 0.0, 0.0], time_from_start: { sec: 5, nanosec: 500 } },
     ]
   }
 }"
--- pick_on.sh
+++ pick_on.sh
@@ -5,8 +5,8 @@ ros2 action send_goal /gripper_controller/follow_joint_trajectory control_msgs/a
     joint_names: [finger_joint1],
     points: [
       { positions: [0.0], time_from_start: { sec: 0, nanosec: 200 } },
-      { positions: [0.1], time_from_start: { sec: 2, nanosec: 200 } },
-      { positions: [0.2], time_from_start: { sec: 4, nanosec: 200 } }
+      { positions: [0.1], time_from_start: { sec: 1, nanosec: 200 } },
+      { positions: [0.2], time_from_start: { sec: 2, nanosec: 200 } }
     ]
   }
 }"
